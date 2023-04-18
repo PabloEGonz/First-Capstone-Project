e31a0c8820c954be316998b768f0dcc9932c16dc
Pablo <ezekielgonzalezr@gmail.com>
2023-04-17 21:22:08 -0600

    Add artist info to an array

---
+++ index.js
@@ -0,0 +1,38 @@
+artist = [
+    {
+        photo: "",
+        name: "",
+        intro: "Belgian singer, songwriter, and producer.",
+        presentation: "Angèle approaches contemporary dance-pop with a classic French pop bent. She went all the way to number one in France and Belgium with her debut album, 2018's Brol."
+    },
+    {
+        photo: "",
+        name: "",
+        intro: "French-Malian singer, and songwriter.",
+        presentation: "After a remarkable debut on social networks and numerous collaborations, she made a breakthrough with the release of her first album, which was certified platinum after just over two years."
+    },
+    {
+        photo: "",
+        name: "",
+        intro: "Belgian singer, rapper, songwriter and producer.",
+        presentation: "He is mostly known for his music blending hip hop and electronic music. Stromae came to wide public attention in 2009 with his song Alors on danse."
+    },
+    {
+        photo: "",
+        name: "",
+        intro: "French musical project formed in Nantes",
+        presentation: "They became popular for the song Amour Plastique, which was released in the September of 2018, which has accumulated more than 107 million views on YouTube as of March 2023."
+    },
+    {
+        photo: "",
+        name: "",
+        intro: " French singer, songwriter, and musician.",
+        presentation: "Her debut studio album, À peu près (2017), received critical acclaim for its mixture of pop and folk music and entered the French album charts at number 91."
+    },
+    {
+        photo: "",
+        name: "",
+        intro: " French singer and songwriter.",
+        presentation: "She collaborated with many musicians on vocals and songwriting prior to releasing her first single, Dernière danse , in December 2013, which reached SNEP Top 2 in France."
+    }
+]
