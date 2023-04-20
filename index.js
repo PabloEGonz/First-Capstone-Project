0d609a1d46a3ab47d90234be41dcb37fcdb58fc4
Pablo <ezekielgonzalezr@gmail.com>
2023-04-19 21:48:08 -0600

    Create object with card info

--- index.js
+++ index.js
@@ -1,38 +1,56 @@
 artist = [
     {
-        photo: "",
-        name: "",
-        intro: "",
-        presentation: ""
+        photo: "images/angele.jpg",
+        name: "Angèle",
+        intro: "Belgian singer, songwriter, and producer.",
+        presentation: "Angèle approaches contemporary dance-pop with a classic French pop bent. She went number one in France and Belgium with her debut album, Brol."
     },
     {
-        photo: "",
-        name: "",
-        intro: "",
-        presentation: ""
+        photo: "images/nakamura.jpg",
+        name: "Aya Nakamura",
+        intro: "French-Malian singer, and songwriter.",
+        presentation: "She made a breakthrough with the release of her first album, which was certified platinum after just over two years."
     },
     {
-        photo: "",
-        name: "",
-        intro: "",
-        presentation: ""
+        photo: "images/stromae.jpg",
+        name: "Stromae",
+        intro: "Belgian singer, rapper, songwriter and producer.",
+        presentation: "He is known for his music blending hip hop and electronic music. Stromae came to wide public attention with his song Alors on danse."
     },
     {
-        photo: "",
-        name: "",
-        intro: "",
-        presentation: ""
+        photo: "images/videoclub.jpg",
+        name: "Video Club",
+        intro: "French musical project formed in Nantes",
+        presentation: "They became popular for the song Amour Plastique, which has accumulated more than 107 million views on YouTube as of March 2023."
     },
     {
-        photo: "",
-        name: "",
-        intro: "",
-        presentation: ""
+        photo: "images/indila.jpg",
+        name: "Indila",
+        intro: "French singer and songwriter.",
+        presentation: "She released her first single, Dernière danse , in December, 2013, which reached SNEP Top 2 in France."
     },
     {
-        photo: "",
-        name: "",
-        intro: "",
-        presentation: ""
+        photo: "images/pomme.jpg",
+        name: "Pomme",
+        intro: "French singer, songwriter, and musician.",
+        presentation: "Her debut studio album, À peu près (2017), received critical acclaim and entered the French album charts at number 91."
     }
-]
+];
+
+const artiContainer = document.getElementsByClassName("artist");
+const img = document.getElementsByClassName("img-fluid");
+const names = document.getElementsByClassName("artist-name");
+const intro = document.getElementsByClassName("intro");
+const cardText = document.getElementsByClassName("paragraph");
+
+window.onload = () => {
+
+    for (let i = 0; i < artiContainer.length; i++) {
+        img[i].src = artist[i].photo;
+        names[i].innerHTML = artist[i].name;
+        intro[i].innerHTML = artist[i].intro;
+        cardText[i].innerHTML = artist[i].presentation;
+    }
+
+
+}
